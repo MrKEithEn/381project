57ea3f58dd505a6a7c61d5f57ed1821b3849b4b5
KEithEn <laukit37@gmail.com>
2022-11-23 14:04:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const dbURI = "mongodb+srv://admin:admin@cluster0.arpzdfb.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(8080))
   .catch(err => console.log(err));
 
 // register view engine
